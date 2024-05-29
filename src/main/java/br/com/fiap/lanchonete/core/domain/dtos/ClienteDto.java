06290122baf70c2be9f40420b36bd30e97000c22
unknown <legasrossini>
2024-05-28 21:35:03 -0300

    ajustes dtos

--- src/main/java/br/com/fiap/lanchonete/core/domain/dtos/ClienteDto.java
+++ src/main/java/br/com/fiap/lanchonete/core/domain/dtos/ClienteDto.java
@@ -4,53 +4,40 @@ public class ClienteDto {
     private String cpf;
     private String nome;
     private String email;
-
-    // Constructor without parameters
-    public ClienteDto() {
+        public ClienteDto() {
     }
 
-    // Constructor with all parameters
     public ClienteDto(String cpf, String nome, String email) {
         this.cpf = cpf;
         this.nome = nome;
         this.email = email;
     }
-
     public static Builder builder() {
         return new Builder();
     }
-
-    // Getters and Setters
     public String getCpf() {
         return cpf;
     }
-
     public void setCpf(String cpf) {
         this.cpf = cpf;
     }
-
     public String getNome() {
         return nome;
     }
-
     public void setNome(String nome) {
         this.nome = nome;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
 
-    // Builder class
     public static class Builder {
         private String cpf;
         private String nome;
         private String email;
-
         public Builder cpf(String cpf) {
             this.cpf = cpf;
             return this;
@@ -65,7 +52,6 @@ public class ClienteDto {
             this.email = email;
             return this;
         }
-
         public ClienteDto build() {
             return new ClienteDto(cpf, nome, email);
         }
